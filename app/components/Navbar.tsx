9016e38f857eba878f7282f375bbe7a1ea47c7ef
DulsonGroup <service@dulsonparfum.com>
2025-03-18 15:32:03 +0000

    Исправлена ошибка с useState

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+"use client";
+
 import { useState } from 'react';
 
 export default function Navbar() {
@@ -6,7 +7,7 @@ export default function Navbar() {
 
   // Функция для отображения сообщения
   const showAccessDenied = () => {
-    setMessage('Sie haben keinen Zugriff.');
+    setMessage('Sie haben keinen Zugriff');
     setTimeout(() => setMessage(''), 3000);
   };
 
@@ -16,14 +17,14 @@ export default function Navbar() {
       <nav className="fixed top-0 left-0 w-full text-white p-4 flex justify-center space-x-4 z-20">
         <a href="https://dulsonparfum.com" target="_blank" rel="noopener noreferrer">Dulson Parfum</a>
         <span className="cursor-pointer" onClick={showAccessDenied}>Dulson Digital</span>
-        <a href="https://dulsongroup.com" target="_blank" rel="noopener noreferrer">Dulson Group</a> 
+        <a href="https://dulsongroup.com" target="_blank" rel="noopener noreferrer">Dulson Group</a>
         <span className="cursor-pointer" onClick={showAccessDenied}>Dulson Art</span>
         <span className="cursor-pointer" onClick={showAccessDenied}>Dulson Immobilien</span>
       </nav>
 
-      {/* Сообщение "У вас нет доступа" */}
+      {/* Сообщение "у вас нет доступа" */}
       {message && (
-        <div className="fixed top-16 left-1/2 transform -translate-x-1/2 bg-red-600 text-white px-4 py-2 rounded">
+        <div className="fixed top-20 left-1/2 transform -translate-x-1/2 bg-red-600 text-white px-4 py-2 rounded">
           {message}
         </div>
       )}
