f23b38f1370aa84225886195cda15ad8a53d2d7b
DulsonGroup <service@dulsonparfum.com>
2025-03-18 07:53:40 +0000

    Обновление навигации

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,6 @@
-import Link from 'next/link';
-
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-gray-900 text-white p-4 flex justify-center space-x-6 z-50">
+    <nav className="fixed top-0 left-0 w-full bg-gray-900 text-white p-4 flex justify-center space-x-4">
       <Link href="/parfum">Dulson Parfum</Link>
       <Link href="/digital">Dulson Digital</Link>
       <Link href="/">Dulson Group</Link> {/* Это главная страница */}
