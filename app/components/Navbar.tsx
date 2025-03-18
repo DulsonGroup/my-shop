576b04c738b4169dd8039621c5170f5b74b2525a
DulsonGroup <service@dulsonparfum.com>
2025-03-18 15:08:14 +0000

    Исправлена ссылка на Dulson Parfum

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,9 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full text-white p-4 flex justify-center space-x-4 z-20">
-      <Link href="/parfum">Dulson Parfum</Link>
+      <a href="https://dulsonparfum.com" target="_blank" rel="noopener noreferrer">
+        Dulson Parfum
+      </a>
       <Link href="/digital">Dulson Digital</Link>
       <Link href="/">Dulson Group</Link> {/* Это главная страница */}
       <Link href="/art">Dulson Art</Link>
