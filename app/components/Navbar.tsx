235170895a7cf8bbdb93524d0c14c42544be3813
DulsonGroup <service@dulsonparfum.com>
2025-03-18 11:16:29 +0000

    Fix import path for Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function Navbar() {
+export default function navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-50 text-white p-4 flex justify-center space-x-4 z-20">
       <Link href="/parfum">Dulson Parfum</Link>
