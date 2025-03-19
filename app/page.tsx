ad9cff413dc265ae5771d3683b447964600a28e5
DulsonGroup <service@dulsonparfum.com>
2025-03-19 22:16:26 +0000

    Обновлено фоновое видео

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,18 @@ import Navbar from './components/Navbar';
 
 export default function Home() {
   return (
-    <div className="relative w-screen h-screen flex flex-col">
+    <div className="relative w-screen h-screen overflow-hidden flex flex-col">
       {/* Фоновое видео */}
-      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black">
-        <video
-          autoPlay
-          loop
-          muted
-          playsInline
-          className="w-full h-full max-w-none object-contain aspect-video"
-        >
-          <source src="/video/dulsongroup_2.mp4" type="video/mp4" />
-          Ваш браузер не поддерживает видео-тег.
-        </video>
-      </div>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source src="/video/Recording_3CCD5C4B2-B5C5-4778-8095-73BDEA14090A.MP4" type="video/mp4" />
+        Ваш браузер не поддерживает видео-тег.
+      </video>
 
       {/* Контейнер для контента поверх видео */}
       <div className="relative z-10 flex flex-col justify-between h-full min-h-screen">
