86fc9141b9faf2ff0fdcad80fbc854eca00e146a
DulsonGroup <service@dulsonparfum.com>
2025-03-19 21:57:40 +0000

    Изменен файл фона: dulsongroup_2.mp4

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover"
       >
-        <source src="/video/dulsongroup.mp4" type="video/mp4" />
+        <source src="/video/dulsongroup_2.mp4" type="video/mp4" />
         Ваш браузер не поддерживает видео-тег.
       </video>
 
