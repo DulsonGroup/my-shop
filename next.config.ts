28c83af93e64ac88baddc4992853965b708b91bb
DulsonGroup <service@dulsonparfum.com>
2025-03-20 11:02:58 +0000

    Закоммичены все изменения

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Включает поддержку статического экспорта
-  trailingSlash: true, // Добавляет слэш в конец URL (исправляет возможные ошибки)
-  images: { unoptimized: true } // Отключает оптимизацию изображений (важно для экспорта)
-};
-
-export default nextConfig;
