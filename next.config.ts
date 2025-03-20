8630d332cc2d3f36daaf3db717763b982c00837b
DulsonGroup <service@dulsonparfum.com>
2025-03-20 10:34:41 +0000

    Добавлен next.config.ts для экспорта

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Включает поддержку статического экспорта
+  trailingSlash: true, // Добавляет слэш в конец URL (исправляет возможные ошибки)
+  images: { unoptimized: true } // Отключает оптимизацию изображений (важно для экспорта)
 };
 
 export default nextConfig;
